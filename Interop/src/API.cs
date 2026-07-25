c1a830d2d950dbf8e486a4c21d8dd3b6f31b0656
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2026-07-25 18:24:26 +0100

    Cleaned up C# side
    Also fixed a bug when no serialized object was provided

--- Interop/src/API.cs
+++ Interop/src/API.cs
@@ -1,6 +1,8 @@
 using System.Runtime.InteropServices;
 using Photino.NET;
 
+using TextEncoding = System.Text.Encoding;
+
 namespace PashaBibko.OpenGitClient;
 
 public static class InteropExports
@@ -8,13 +10,12 @@ public static class InteropExports
     [UnmanagedCallersOnly(EntryPoint = "OpenGitClientInterop_BindMessageReceiver")]
     public static unsafe void BindMessageReceiver(delegate* unmanaged[Cdecl]<byte*, byte*, void> function)
     {
-        AppContext.MessageReceivedCallback = (funcName, serializeObject) =>
+        AppContext.MessageReceivedCallback = (funcName, serializedObject) =>
         {
-            byte[] funcNameBytes = System.Text.Encoding.UTF8.GetBytes(funcName);
-            byte[] serializedObjectBytes = System.Text.Encoding.UTF8.GetBytes(serializeObject);
-
-            fixed (byte* pFuncName = funcNameBytes)
-            fixed (byte* pSerializedObject = serializedObjectBytes)
+            fixed (byte* pFuncName = TextEncoding.UTF8.GetBytes(funcName))
+            fixed (byte* pSerializedObject = serializedObject == null
+                       ? null
+                       : TextEncoding.UTF8.GetBytes(serializedObject))
             {
                 function(pFuncName, pSerializedObject);
             }
@@ -37,5 +38,18 @@ public static class InteropExports
             .Center()
             .RegisterWebMessageReceivedHandler(MessageHandler.Handle)
             .WaitForClose();
+    }
+}
+
+internal static class MessageHandler
+{
+    public static void Handle(object _, string message)
+    {
+        string[] parts = message.Split('|', 2);
+        AppContext.MessageReceivedCallback
+        (
+            parts[0],
+            parts.Length > 1 ? parts[1] : null
+        );
     }
 }
