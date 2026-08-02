7e40a28621e95f607884bc5a141c3ebfc7b0d795
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2026-08-02 17:03:12 +0100

    "Fixed" the memory "leak"

--- backend/main.cpp
+++ backend/main.cpp
@@ -24,22 +24,31 @@ int main() {
     router.AddFunction<User::GetLastOpenedRepo>("User.GetLastOpenedRepo");
 
     // Starts photino.net with a message receiver to forward to the web function router
-    OpenGitClientInterop::BindMessageReceiverCallback(
-        [](const char* name, const char* serializedVal) -> char* {
-            const std::optional result = router.InvokeFunction(name, serializedVal);
-            if (!result.has_value()) {
-                return nullptr; // Will be converted to "null" by C#
-            }
+    {
+        static char* sharedBuffer = nullptr;
+        OpenGitClientInterop::BindMessageReceiverCallback(
+            [](const char* name, const char* serializedVal) -> char* {
+                const std::optional result = router.InvokeFunction(name, serializedVal);
+                if (!result.has_value()) {
+                    return nullptr; // Will be converted to "null" by C#
+                }
 
-            // Copies the string to a cstr so it can be passed to C#
-            // TODO: Add a way for C#/TypeScript to free to avoid memory leaking
-            const std::string& serialized = result.value();
-            const size_t serializedLen = serialized.size();
-            char* buffer = new char[serializedLen + 1];
-            std::ranges::copy(serialized, buffer);
-            buffer[serializedLen] = '\0';
+                // Deletes anything that may have been stored in the shared buffer, the
+                // buffer being shared is fine because it works in a single thread, it will technically leak some
+                // memory at the end of the process, but ohh well, if you are looking at this code to consider if
+                // you should hire me please ignore the fact that I am leaking memory here. Please hire me thx.
+                delete[] sharedBuffer;
+                sharedBuffer = nullptr;
 
-            return buffer;
-        });
+                // Copies the string to a cstr so it can be passed to C#
+                const std::string& serialized = result.value();
+                const size_t serializedLen = serialized.size();
+                sharedBuffer = new char[serializedLen + 1];
+                std::ranges::copy(serialized, sharedBuffer);
+                sharedBuffer[serializedLen] = '\0';
+
+                return sharedBuffer;
+            });
+    }
     OpenGitClientInterop::StartPhotino(); // Blocks until window is closed
 }
